e657bf38327f112a2f49d9099e3b90c9413b4dcf
Nurmukhammad Sunatullaev <nurmukhammad.sunatullaev@gmail.com>
2022-07-11 17:46:12 +0500

    Configuration Changed

--- ResourceService/src/test/java/com/epam/microservice/resourceservice/ResourceServiceApplicationTests.java
+++ ResourceService/src/test/java/com/epam/microservice/resourceservice/ResourceServiceApplicationTests.java
@@ -1,10 +1,25 @@
 package com.epam.microservice.resourceservice;
 
+import com.epam.microservice.resourceservice.controller.ResourceController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ResourceServiceApplicationTests {
+
+    @Autowired
+    private ResourceController controller;
+
+    @Test
+    public void resourceControllerTest(){
+        assertThat(controller).isNotNull();
+    }
 
 
 }
